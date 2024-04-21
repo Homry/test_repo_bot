7c1b932eb042877058cbe0aaae431165add9dbd4
zwphyro <tofumaks@gmail.com>
2024-04-21 15:18:05 +0300

    try 2: success

--- .github/actions/run-metrics/entrypoint.sh
+++ .github/actions/run-metrics/entrypoint.sh
@@ -4,7 +4,4 @@ cd $GITHUB_WORKSPACE/$ACTION_WORKSPACE/src
 
 cp -r $GITHUB_WORKSPACE/pull-request-data . 
 
-pwd
-ls
-
 python3 main.py
--- .github/actions/run-metrics/src/main.py
+++ .github/actions/run-metrics/src/main.py
@@ -15,31 +15,37 @@ if __name__ == "__main__":
     print(parsed_json)
 
     for el in parsed_json:
+        if not el["correct"]:
+            continue
+
         for file in el["files"]:
-            if "main.py" in file["path"]:
-                path = "pull-request-data/"+file["path"]
-                path = path.replace("/",".")
-                obj = __import__(path[:-3], fromlist=[None])
+            if "main.py" not in file["path"]:
+                continue
 
-                eva = ModelEvaluator(obj.model, DatasetInterface("./src/action/datasets/train-scene/train.csv",
-                                                                 "./src/action/datasets/train-scene/train/"),
-                                     64, "./src/action/datasets/train-scene/train.csv")
+            path = "pull-request-data/"+file["path"]
+            path = path.replace("/",".")
+            obj = __import__(path[:-3], fromlist=[None])
 
-                # Evaluate the model
-                metrics, fpr, tpr = eva.evaluate()
+            eva = ModelEvaluator(obj.model, DatasetInterface("./action/datasets/train-scene/train.csv",
+                                                             "./action/datasets/train-scene/train/"),
+                                 64, "./action/datasets/train-scene/train.csv")
 
-                # Print evaluation metrics
-                interpretation = ["Average Precision", "Average Accuracy", "Average Recall", "Average F1-score", "Average ROC-AUC"]
-                for index, metric in enumerate(metrics.tolist()):
-                    print(f"{interpretation[index]}: {metric}")
+            # Evaluate the model
+            metrics, fpr, tpr = eva.evaluate()
 
-                # Plot ROC curve
-                plt.figure(figsize=(8, 6))
-                plt.plot(fpr, tpr, color='blue', lw=2, label='ROC curve')  # Use a contrasting color and thicker line
-                plt.plot([0, 1], [0, 1], color='gray', lw=1, linestyle='--')  # Diagonal line
-                plt.xlabel('False Positive Rate')
-                plt.ylabel('True Positive Rate')
-                plt.title('Receiver Operating Characteristic (ROC) Curve')
-                plt.legend(loc='lower right')
-                plt.grid(True)  # Add gridlines
-                plt.show()
+            # Print evaluation metrics
+            interpretation = ["Average Precision", "Average Accuracy", "Average Recall", "Average F1-score", "Average ROC-AUC"]
+            for index, metric in enumerate(metrics.tolist()):
+                el["comment"] += f"{interpretation[index]}: {metric}\n"
+                print(f"{interpretation[index]}: {metric}")
+
+            # Plot ROC curve
+            plt.figure(figsize=(8, 6))
+            plt.plot(fpr, tpr, color='blue', lw=2, label='ROC curve')  # Use a contrasting color and thicker line
+            plt.plot([0, 1], [0, 1], color='gray', lw=1, linestyle='--')  # Diagonal line
+            plt.xlabel('False Positive Rate')
+            plt.ylabel('True Positive Rate')
+            plt.title('Receiver Operating Characteristic (ROC) Curve')
+            plt.legend(loc='lower right')
+            plt.grid(True)  # Add gridlines
+            plt.show()
