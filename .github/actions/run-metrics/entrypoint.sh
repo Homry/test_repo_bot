70c988b80ec0bd1101b546ad5bd31c46e22e7f01
zwphyro <tofumaks@gmail.com>
2024-04-22 21:51:30 +0300

    added latest run-metrics

--- .github/actions/run-metrics/entrypoint.sh
+++ .github/actions/run-metrics/entrypoint.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+# Функция для обхода папок
+traverse() {
+    for file in "$1"/*; do
+      for dir in ${file}/*; do
+      # Проверяем, является ли текущая папка директорией
+        if [ -d "$dir" ]; then
+        # Заходим в папку src и скачиваем веса из файла weights_link.txt, если он там есть
+          if [ -f "$dir/src/weights_link.txt" ]; then
+            gdown --fuzzy $(cat ${dir}/src/weights_link.txt) -O ${dir}/src/weights.pth
+          fi
+        fi
+      done
+    done
+}
+
+# Вызываем функцию для обхода и поиска файла
+traverse $GITHUB_WORKSPACE/pull-requests-data
+
 cd $GITHUB_WORKSPACE/$ACTION_WORKSPACE/src
 
 cp -r $GITHUB_WORKSPACE/pull-request-data . 
